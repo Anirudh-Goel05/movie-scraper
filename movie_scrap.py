dd456e16172ffc05d53b1c689e547b542951b248
anirudh <ani050599@gmail.com>
2019-03-07 20:11:44 +0530

    expaned to multiple pages

--- movie_scrap.py
+++ movie_scrap.py
@@ -10,29 +10,32 @@ headers = "Movie-name, Quality\n"
 
 f.write(headers)
 
-# import urllib.request
-my_url = Request('https://www1.yesmovies.gg/movie/filter/movie/all/all/all/all/latest/', headers={'User-Agent': 'Mozilla/5.0'})
+print('fetching data from the website ...')
+for i in range(1,6):
+	# import urllib.request
+	raw_url = 'https://www1.yesmovies.gg/movie/filter/movie/all/all/all/all/latest/' + "?page=" + str(i)
+	my_url = Request(raw_url, headers={'User-Agent': 'Mozilla/5.0'})
 
-# Opening a new connection and grabing the page's html
-uClient = urlopen(my_url)
+	# Opening a new connection and grabing the page's html
+	uClient = urlopen(my_url)
 
-page_html = uClient.read()
-uClient.close()
+	page_html = uClient.read()
+	uClient.close()
 
-# Parsing the html page
-page_soup = soup(page_html,'html.parser')
+	# Parsing the html page
+	page_soup = soup(page_html,'html.parser')
 
-#Finding all containers from html dom examination
-containers = page_soup.findAll("div",{'class':"ml-item"})
+	#Finding all containers from html dom examination
+	containers = page_soup.findAll("div",{'class':"ml-item"})
 
-for container in containers:
-	info = container.text.split('\n')
-	info = [x for x in info if x != '']
-	quality = info[0].replace(' ','-')
-	quality = quality.replace(',','-')
-	title = info[1].replace(' ','-')
-	title = title.replace(',','-')
-	# print(quality + " " + title)
-	f.write(title + "," + quality + "\n")
+	for container in containers:
+		info = container.text.split('\n')
+		info = [x for x in info if x != '']
+		quality = info[0].replace(' ','-')
+		quality = quality.replace(',','-')
+		title = info[1].replace(' ','-')
+		title = title.replace(',','-')
+		f.write(title + "," + quality + "\n")
 
-f.close()
+print('mission accomplished ...')
+f.close()	
