06aa2216f381eb88fc54c4f3112a1a499358e04b
anirudh <ani050599@gmail.com>
2019-03-07 20:31:25 +0530

    bug fixed

--- indian_movie_scrap.py
+++ indian_movie_scrap.py
@@ -10,14 +10,27 @@ headers = "Movie-name, Quality\n"
 
 f.write(headers)
 
+while True:	
+	y = int(input('Enter the number of pages till you want to search:'))
+	if y <= 0:
+		print('Page number must be >= 1')
+	else:
+		y += 1
+		break	
+
+
 print('fetching data from the website ...')
-for i in range(1,5):
+for i in range(1,y):
 	# import urllib.request
 	raw_url = 'https://www1.yesmovies.gg/country/india/' + "?page=" + str(i)
 	my_url = Request(raw_url, headers={'User-Agent': 'Mozilla/5.0'})
 
 	# Opening a new connection and grabing the page's html
-	uClient = urlopen(my_url)
+	try:
+		uClient = urlopen(my_url)
+	except:
+		break
+
 	page_html = uClient.read()
 	uClient.close()
 
